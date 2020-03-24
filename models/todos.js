4c374345d2f4ecda6f5768b6564d7cb148ad51e9
chiharakenta <chiharakenta0519@gmail.com>
2020-03-24 16:24:38 +0900

    add model validation

--- models/todos.js
+++ models/todos.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const todos = sequelize.define('todos', {
-    content: DataTypes.STRING
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     underscored: true,
   });
