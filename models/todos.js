ff2106f7d264b05d415622c42dcbbb9adaf0e350
chiharakenta <chiharakenta0519@gmail.com>
2020-03-25 13:15:41 +0900

    add relation

--- models/todos.js
+++ models/todos.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
   todos.associate = function(models) {
-    // associations can be defined here
+    todos.belongsTo(models.category);
   };
   return todos;
 };
