4d2c00b43b340b1f60854ffc68a972c531f9dfe5
chiharakenta <chiharakenta0519@gmail.com>
2020-03-25 13:28:42 +0900

    fix not equal column name error

--- models/category.js
+++ models/category.js
@@ -2,7 +2,9 @@
 module.exports = (sequelize, DataTypes) => {
   const category = sequelize.define('category', {
     name: DataTypes.STRING
-  }, {});
+  }, {
+    underscored: true,
+  });
   category.associate = function(models) {
     category.hasMany(models.todos);
   };
