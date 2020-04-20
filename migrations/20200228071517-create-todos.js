e512cf81c4fa7d154dc5b8c25e4c1fa70c889e75
chiharakenta <chiharakenta0519@gmail.com>
2020-04-20 14:27:42 +0900

    fix migration

--- migrations/20200228071517-create-todos.js
+++ migrations/20200228071517-create-todos.js
@@ -14,13 +14,11 @@ module.exports = {
       },
       created_at: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        type: Sequelize.DATE
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        type: Sequelize.DATE
       }
     });
   },
